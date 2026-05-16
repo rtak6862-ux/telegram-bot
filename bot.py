df3106c725fdde1da695ff3a83b0ff4fea361310
rtak6862-ux <rtak6862@gmail.com>
2026-05-16 20:09:07 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 
 from telegram.ext import Application, CommandHandler
 
-TOKEN = "8772201536:AAG6TpU04rkcKjnV_dQcpkrqfmUtK42cYpg"
+TOKEN = "8870873326:AAHBvCp1Koy45_TxAC2_pkxMKWGI815vdxw"
 
 async def start(update, context):
-    await update.message.reply_text("سلام")
+    await update.message.reply_text("ربات روشن است")
 
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+
+print("STARTED")
 
 app.run_polling()
